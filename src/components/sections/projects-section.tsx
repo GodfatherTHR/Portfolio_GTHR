cf2649e743fafe880d215d1124f38b4c015527b6
Godfather THR <justvlog4all@gmail.com>
2025-06-30 19:09:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/projects-section.tsx
+++ src/components/sections/projects-section.tsx
@@ -21,7 +21,7 @@ const projectImageMap: { [key: string]: string } = {
   'proj1': 'https://images.unsplash.com/photo-1743336751210-5205213290ca?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw5fHxJbnRlbGxpZ2VudCUyMEJsb2NrY2hhaW4lMjBFUlAlMjBNb2R1bGV8ZW58MHx8fHwxNzUxMTc4ODQwfDA&ixlib=rb-4.1.0&q=80&w=1080',
   'proj2': 'https://images.unsplash.com/photo-1716436329836-208bea5a55e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw1fHxuOG4lMjBhdXRvbWF0aW9ufGVufDB8fHx8MTc1MTI4NDMyNXww&ixlib=rb-4.1.0&q=80&w=1080',
   'proj3': 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyMHx8RW50ZXJwcmlzZSUyMERhdGElMjBBbmFseXRpY3MlMjBEYXNoYm9hcmR8ZW58MHx8fHwxNzUxMjg0MzYzfDA&ixlib=rb-4.1.0&q=80&w=1080',
-  'proj4': 'https://images.unsplash.com/photo-1516383274235-5f42d6c6426d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxM3x8UmVzZWFyY2glMjBEYXRhJTIwfGVufDB8fHx8MTc1MTMxMDM0MXww&ixlib=rb-4.1.0&q=80&w=1080',
+  'proj4': 'https://images.unsplash.com/photo-1647529734891-b1d1d2b93bd3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNXx8c3Rvcnl0ZWxsaW5nJTIwbWFzdGVyY2xhc3N8ZW58MHx8fHwxNzUxMzEwNTIyfDA&ixlib=rb-4.1.0&q=80&w=1080',
 };
 
 export default async function ProjectsSection() {
