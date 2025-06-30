e5b74bbd7b0a3fa048459ac9c3e964c2dc473b89
Godfather THR <justvlog4all@gmail.com>
2025-06-30 18:18:26 +0000

    500: INTERNAL_SERVER_ERROR Code: MIDDLEWARE_INVOCATION_FAILED ID: bom1::

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+
 import { NextResponse, type NextRequest } from "next/server";
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 
@@ -17,21 +18,11 @@ export async function middleware(request: NextRequest) {
           return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({ name, value, ...options });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
+          // If the cookie is set, update the response's cookies.
           response.cookies.set({ name, value, ...options });
         },
         remove(name: string, options: CookieOptions) {
-          request.cookies.set({ name, value: "", ...options });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
+          // If the cookie is removed, update the response's cookies.
           response.cookies.set({ name, value: "", ...options });
         },
       },
