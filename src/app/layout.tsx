394e71a02cfa78bc473e18b0c26f5b17a3a48e13
Godfather THR <justvlog4all@gmail.com>
2025-07-27 00:32:31 +0000

    User-agent: * Allow: /
    <meta name="robots" content="index, follow">
    
    Go

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,12 @@ export async function generateMetadata(): Promise<Metadata> {
       default: 'Shariful Haque | Portfolio',
     },
     description: 'Personal portfolio of Shariful Haque, a passionate developer.',
-    other: {
-      'google-site-verification': 'c15UaCh43Q5i2d1-pP1IF-kS_t1X1-g-gR-g-g'
+    robots: {
+      index: true,
+      follow: true,
+    },
+    verification: {
+      google: '5UsrGLgWGhkR1HVyuJxxkcFXubmeePr6ozOWgTytdAM',
     }
   }
 }
