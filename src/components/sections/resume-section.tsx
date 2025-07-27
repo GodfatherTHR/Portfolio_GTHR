29369cdcb183a4f0549a4e591688b1640a04ed6a
Godfather THR <justvlog4all@gmail.com>
2025-07-27 13:08:43 +0000

    Skills all box font color need to be white.

--- src/components/sections/resume-section.tsx
+++ src/components/sections/resume-section.tsx
@@ -1,3 +1,4 @@
+
 import { createClient } from "@/lib/supabase/server";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Briefcase, GraduationCap, Download, Terminal } from 'lucide-react';
